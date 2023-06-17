299ae58467e3f0b9af9c726902ee552736d18011
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-06-17 18:40:35 -0300

    feat: list games

--- src/graphql/modules/games/resolvers.js
+++ src/graphql/modules/games/resolvers.js
@@ -2,6 +2,12 @@ import Category from "../../../models/Category";
 import Game from "../../../models/Game";
 
 export default {
+	Game: {
+		category: async (game) => await Category.findById(game.category),
+	},
+	Query: {
+		games: async () => await Game.find({}),
+	},
 	Mutation: {
 		createGame: (_, { data }) => Game.create(data),
 	},
